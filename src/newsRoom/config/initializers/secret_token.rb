a0766252d50d27fd04846787edccbc7e24c9f2d7
avasquezg <avasquezg@gmail.com>
2012-10-27 22:37:49 -0300

    Agregada nueva version del proyecto sin ActiveRecord

---
+++ src/newsRoom/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsRoom::Application.config.secret_token = 'b0fced365600b5ed9c7b5f5cc2c51889844301192ba23f84e6a777056e396c270a87ccd2357d5830b72936487c29c508806e1d95e17c2afadab5446bdeca8982'
