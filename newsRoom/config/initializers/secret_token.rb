955b1ddb6c419f1655ee43669d653ad0ecd0a00b
Hans <hanstrax@gmail.com>
2012-10-28 18:08:16 -0300

    directory change, routes updated.

--- newsRoom/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NewsRoom::Application.config.secret_token = 'cf1074d89289d00fabc9bcd883b252132b834cb3cada3eec7b873f08a08e9535e462e8461557ff06b4929d7bd6e86472c85bf130c05848188355cefacc5b242d'
