acda859bd9e764dfad8a1cf27a3668c999eec981
Pablo <pablo@ubuntu.(none)>
2012-11-12 17:46:09 -0300

    tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,17 +3,27 @@ require 'capybara/rspec'
 
 describe User do
   before (:each) do
+    @user = User.new
     #@current_user = build(:user)
     #current_user = @current_user
     #@current_user.save
   end
 
-  describe "GET /users" do
-    
+  describe "GET /users" do    
     it "get to index" do
       get users_path
       response.status.should be(200)
     end
-
   end
+
+  it "Should not create user with no email" do  
+      @user = build(:user) 
+      visit new_user_path  
+      fill_in "user_password", :with => @user.password
+      fill_in "user_password_confirmation", :with => @user.password_confirmation
+      fill_in "user_name", :with => @user.name
+      click_button "Create User"
+      page.should_not have_content("User was successfully created.")
+  end  
+
 end
