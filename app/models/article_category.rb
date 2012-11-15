6df981e86761638da29be807926266f349ea8b1c
Hans <hanstrax@gmail.com>
2012-11-15 00:27:07 -0300

    tests for the user_category model

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -9,7 +9,7 @@ class ArticleCategory
   def self.construct(article, category_name)
     if category_name.class.eql?(String)
   	  cat = Category.construct(category_name)
-    else #assumming it is an ArticleCategory object
+    else #assumming it is a Category object
       cat = category_name
     end
     if article.class.eql?(Article)
--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -9,13 +9,19 @@ class UserCategory
   def self.construct(user, category_name)
   	if category_name.class.eql?(String)
       cat = Category.construct(category_name)
-    else #assumming it is an ArticleCategory object
+    else #assumming it is a Category object
       cat = category_name
     end
-    if user.class.eql?(User)
-      #ArticleCategory.
-      create(user: user, category: cat)
-      true
+
+    if user.class.eql?(User) 
+      if user.id.blank?
+        #it should not create a new user
+        false
+      else
+        #ArticleCategory.
+        create(user: user, category: cat)
+        true
+      end
     else
       false
     end
