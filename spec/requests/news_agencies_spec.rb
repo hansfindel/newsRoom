fb647dbce12bff63b87ab19c65bf7672d4f205ce
Pablo <pablo@ubuntu.(none)>
2012-11-16 17:37:56 -0300

    added newsagency tests

--- spec/requests/news_agencies_spec.rb
+++ spec/requests/news_agencies_spec.rb
@@ -5,7 +5,7 @@ describe "News_Agencies" do
 
 	before :each do
 		NewsAgency.destroy_all
-		@news_agency = NewsAgency.new
+		@news_agency = build(:news_agency)
 	end
 
 	describe "GET /news_agencies" do
@@ -13,6 +13,40 @@ describe "News_Agencies" do
     	  get news_agencies_path
     	  response.status.should be(200)
     	end
+	end
+
+	describe "creating agencies" do 
+
+		it "New News agency link should redirect to New News agency path" do 
+	      visit news_agencies_path
+	      click_link "New News agency"
+	      current_path.should == new_news_agency_path
+	    end
+
+	    it "Should not create agency with no feed_url" do  
+	        visit new_news_agency_path 
+	        fill_in "news_agency_name", :with => @news_agency.name
+	        click_button "Create News agency"
+	        page.should have_content("must be present")
+	        page.should_not have_content("News agency was successfully created.")
+	    end  
+
+	    it "Should not create agency with no name" do  
+	        visit new_news_agency_path 
+	        fill_in "news_agency_feed_url", :with => @news_agency.feed_url
+	        click_button "Create News agency"
+	        page.should have_content("must be present")
+	        page.should_not have_content("News agency was successfully created.")
+	    end  
+
+	   	it "Should create agency" do  
+	        visit new_news_agency_path
+	        fill_in "news_agency_name", :with => @news_agency.name
+	        fill_in "news_agency_feed_url", :with => @news_agency.feed_url
+	        click_button "Create News agency"
+	        page.should have_content("News agency was successfully created.")
+	    end  
+
 	end
 
 end
