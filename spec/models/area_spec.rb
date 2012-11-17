70fab018bba917a73a7fbe86d7df41a7feb11844
Pablo <pablo@ubuntu.(none)>
2012-11-17 15:02:45 -0300

    area model tests

---
+++ spec/models/area_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe User do
+	before :each do
+		Area.destroy_all
+		@area = Area.new
+	end
+
+	describe "#new" do
+	    it "takes no parameters and returns an Area object" do
+	        @area.should be_an_instance_of Area
+	    end
+	end
+
+	describe "test the validations" do
+		it "cannot be saved, no name" do 
+			@area.save.should be_false
+		end
+
+		it "cannot be saved, it has already taken name" do
+			@area = build(:area) 
+			@area.save.should be_true
+			newarea = Area.new
+			newarea = build(:area)
+			newarea.save.should be_false
+		end
+
+		it "can be saved" do
+			@area = build(:area) 
+			@area.save.should be_true
+		end
+	end
+end
