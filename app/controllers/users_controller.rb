13b8ccd3d5528d72778923e101840fca07d7178f
Hans <hanstrax@gmail.com>
2012-11-18 18:34:05 -0300

    logs in when new user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     respond_to do |format|
       if @user.save
+        session[:user_id] = user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
