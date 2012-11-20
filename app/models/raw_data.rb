c84c8d2f718efb51db6bc35c1f381e7c79a43764
avasquezg <avasquezg@gmail.com>
2012-11-19 23:16:53 -0300

    Agregado guardar a disco feeds

--- app/models/raw_data.rb
+++ app/models/raw_data.rb
@@ -1,4 +1,6 @@
 class RawData
+  require 'uri'
+  require 'json'
   #include Mongoid::Document
   #field :value, type: Feed
   #field :data, type: Array
@@ -10,20 +12,29 @@ class RawData
   	#save it on the data base
     if data.nil? 
       #value = "No new feeds"
+
       value = []
     else
-      data = get_data data
-    	data.entries.each do |feed|
-    		unless exists? :guis => feed.id
-          Article.create!(
-            :headline => entry.title,
-            :deck => entry.summary,
-            :url => entry.url,
-            :published_on => entry.published_at,
-            :guid => entry.guid
-            )
+     	data.entries.each do |entry|
+      puts entry.title
+      a = Article.new(
+          :headline                     => entry.title,
+          :deck                         => entry.summary,
+          :url                          => entry.url,
+          :published_on                 => entry.published,
+          :guid                         => entry.id,
+          :category_names               => entry.categories,
+          :is_published                 => false,
+          :editors_grade                => 0,
+          :chief_editor_grade           => 0, 
+          :chief_editor_country_grade   => 0
+        )
+        my_uri = URI.parse(entry.id)
+    	  File.open("public/feeds/"+my_uri.host+entry.id.split('/').last+".json", "w") do |f|
+          f. << a.to_json
         end
-    	end
+        value << data
+      end
     end
   end
 
