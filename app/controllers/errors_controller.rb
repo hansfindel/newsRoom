760f85219d64c520df688646c5f23e30d08a772a
Hans <hanstrax@gmail.com>
2012-11-20 15:44:07 -0300

    answers 42

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -7,4 +7,12 @@ class ErrorsController < ApplicationController
       format.json { render json: {} }
     end
   end
+
+  def error
+  	respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: 42 }
+    end
+  end
+
 end
