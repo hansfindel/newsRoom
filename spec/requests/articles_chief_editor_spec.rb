c5d9dd595c4850205d2237128e78b0994ba50635
Diego <diego@ubuntu.(none)>
2012-11-22 03:13:06 -0300

    Some tests added

--- spec/requests/articles_chief_editor_spec.rb
+++ spec/requests/articles_chief_editor_spec.rb
@@ -34,4 +34,79 @@ describe Article do
 
     end
 
+     it "can qualify articles" do
+      login_as_editor
+      click_link "Editor Evaluation"
+      page.should have_content("Noticias por clasificar")
+    end
+
+    it "should edit himself" do
+      get log_out_path 
+      editor ||= (User.where(role: 4).first || create(:editor))
+      visit log_in_path
+      fill_in "email", :with => editor.email
+      fill_in "password", :with => "1234"
+      click_button "Log in"
+      
+      visit edit_user_path(editor.id)
+
+      fill_in "user_name", :with => 'NewName'
+      click_button "Update User"
+      page.should have_content("User was successfully updated.")
+    end
+
+    it "should not edit another user" do
+      get log_out_path 
+      login_as_editor
+
+      @user = build(:user)
+      @user.save.should be_true
+      
+      visit edit_user_path(@user.id)
+      
+      page.should have_content("No tienes los permisos necesarios")
+    end
+
+    it "should edit his slaves" do
+      get log_out_path 
+      editor ||= (User.where(role: 8).first || create(:chief_editor))
+      visit log_in_path
+      fill_in "email", :with => editor.email
+      fill_in "password", :with => "1234"
+      click_button "Log in"
+      
+      @user = build(:user)
+      @user.save.should be_true
+
+      @user.setBoss(editor.id)
+
+      visit edit_user_path(@user.id)
+
+      fill_in "user_name", :with => 'NewName'
+      click_button "Update User"
+      page.should have_content("User was successfully updated.")
+    end
+
+    it "should not enter to chief editor country evaluation area" do
+      get log_out_path 
+      login_as_editor
+
+      visit chief_editors_country_path
+      
+      page.should have_content("No tienes los permisos necesarios")
+    end
+
+     it "can create new areas" do
+      Area.destroy_all
+      login_as_editor
+      visit areas_path
+      page.should have_content("New Area")
+      click_link "New Area"
+      page.should have_content("New area")
+      fill_in "area_name", :with => "nombreArea" 
+      click_button "Create Area"
+      page.should have_content("Area was successfully created.")
+      page.should have_content("nombreArea")
+    end
+
 end
