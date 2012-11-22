8bc36cf224ee4c4742f056bbf348d2d472275402
Francisco Caiceo <jfcaiceo55@gmail.com>
2012-11-22 02:14:47 -0300

    tests editor agregados

--- spec/requests/articles_editor_spec.rb
+++ spec/requests/articles_editor_spec.rb
@@ -13,5 +13,42 @@ describe Article do
     end
   end
 
+  describe "editors permissons" do
+    it "can populate by glutton" do
+      get log_out_path 
+      login_as_editor
+      
+      visit store_glutton_feeds_path
+      page.should have_content("New feeds were fetched and processed")
+    end
+
+    it "should edit himself" do
+      get log_out_path 
+      editor ||= (User.where(role: 4).first || create(:editor))
+      visit log_in_path
+      fill_in "email", :with => editor.email
+      fill_in "password", :with => "1234"
+      click_button "Log in"
+      
+      visit edit_user_path(editor.id)
+
+      fill_in "user_name", :with => 'NewName'
+      click_button "Update User"
+      page.should have_content("User was successfully updated.")
+    end
+
+    it "should not edit another user" do
+      get log_out_path 
+      login_as_editor
+
+      @user = build(:user)
+      @user.save.should be_true
+      
+      visit edit_user_path(@user.id)
+      
+      page.should have_content("No tienes los permisos necesarios")
+    end
+
+  end
 
 end
