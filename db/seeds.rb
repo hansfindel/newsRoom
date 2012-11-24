46582f11229585a23cf0edf26bc46d45de5357d6
avasquezg <avasquezg@gmail.com>
2012-11-24 15:29:38 -0300

    Mejorando bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
 
-roles = User::ROLES #arreglo con los posibles roles de los usuarios
+#roles = User::ROLES #arreglo con los posibles roles de los usuarios
 
 soledad = User.last
 
 #reuter = NewsAgency.create!(name:"IPS News", feed_url:"http://feeds.mashable.com/mashable")
 
-reuter = NewsAgency.create!(name:"IPS News", feed_url:"www.ipsnews.net", is_api: false)
+#reuter = NewsAgency.create!(name:"IPS News", feed_url:"www.ipsnews.net", is_api: false)
 #datos de la API de NYTimes
 #body -> story
 #date -> published on
@@ -24,13 +24,13 @@ reuter = NewsAgency.create!(name:"IPS News", feed_url:"www.ipsnews.net", is_api:
 #results -> results
 #url
 
-feedzilla = NewsAgency.create!(name: "Feedzilla", feed_url:"http://api.feedzilla.com/v1/categories/21/articles.json", is_api: true, api_values:{:story, '',:results => 'articles', :headline => 'title', :deck => 'summary', :published_on => 'publish_date', :url => 'url}' )
+#feedzilla = NewsAgency.create!(name: "Feedzilla", feed_url:"http://api.feedzilla.com/v1/categories/21/articles.json", is_api: true, api_values:{:story =>'body',:results => 'articles', :headline => 'title', :deck => 'summary', :published_on => 'publish_date', :url => 'url'} )
 
 
-#900.times do
-#	soledad.articles.create(headline: Faker::Lorem.sentence(word_count = 4), 
-#		deck: Faker::Lorem.paragraph(sentence_count = 2), 
-#		story: Faker::Lorem.paragraph(sentence_count = 4), is_published: true)
-#end
+900.times do
+	soledad.articles.create(headline: Faker::Lorem.sentence(word_count = 4), 
+		deck: Faker::Lorem.paragraph(sentence_count = 2), 
+		story: Faker::Lorem.paragraph(sentence_count = 4), is_published: true)
+end
 
 
